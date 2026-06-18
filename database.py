5885ae8a461ac535c3fec7f58e2ffd42ffa849a1
Caio Farias <caiovv1@outlook.com>
2026-06-18 11:07:06 -0400

    update

--- database.py
+++ database.py
@@ -17,7 +17,7 @@ class Database:
             print("ERRO AO CONECTAR AO BANCO DE DADOS!!")
 
     def insert_produto(self):
-        param = ("Coca-cola", "refri", 12.00, 50)
+        param = ("Arroz", "alimento", 10.00, 20)
         self.conectar()
         try:
             self.cursor.execute("INSERT INTO produto (nome,tipo,preco,qtde_estoque) VALUES (%s,%s,%s,%s)",param)
@@ -27,7 +27,81 @@ class Database:
         except Exception as error:
             print(error)
 
+        finally:
+            self.disconnect()
+
+
+    def select_produto(self):
+        self.conectar()
+        try:
+            self.cursor.execute("SELECT * FROM produto; ")
+            produtos = self.cursor.fetchall()  ###Converter o objeto do banco para uma lista de dicionarios
+            for prod in produtos:
+                print(f" id: {prod[0]} / nome: {prod[1]} / preco: {prod[3]} ")
+        except Exception as error:
+            print(error)
+
+        finally:
+            self.disconnect()    
+
+
+    def select_produto_by_id(self, id_produto):
+        self.conectar()
+        try:
+            self.cursor.execute(f"SELECT * FROM produto WHERE id_produto = {id_produto}; ")
+            prod = self.cursor.fetchone()  ###Converter o objeto do banco para uma lista de dicionarios
+            return list(prod)
+        
+        except Exception as error:
+            print(error)
+
+        finally:
+            self.disconnect()
+
+
+    def update_produto(self, id_prod):
+        self.conectar()
+        try:
+            prod = self.select_produto_by_id(id_prod)
+            print(prod)
+            prod[1] = input("Digite o nome: ")
+            prod[2] = input("Digite o tipo: ")
+            prod[3] = float(input("Digite o preço: "))
+            prod[4] = int(input("Digite a quantidade: "))
+            print(prod)
+            self.cursor.execute(f"""
+                                UPDATE produto SET nome = '{prod[1]}',
+                                tipo = '{prod[2]}', preco = '{prod[3]}',
+                                qtde_estoque = '{prod[4]}'
+                                WHERE id_produto = '{prod[0]}';
+                                """)
+            self.conn.commit()
+            print("PRODUTO ATUALIZADO COM SUCESSO!!!")
+        except:
+            pass
+
+        finally:
+            self.disconnect()
+
+
+    def delete_produto(self, id_produto):
+        self.conectar()
+        try:
+            self.cursor.execute(f" DELETE FROM produto WHERE id_produto={id_produto}; ")
+            self.conn.commit()
+            print("DELETADO COM SUCESSO!")
+        except Exception as error:
+            print(error)
+
+        finally:
+            self.disconnect()
+
+    def disconnect(self):
+        if self.conn.is_connected():
+            self.cursor.close()
+            self.conn.close()
+            print("CONEXÃO ENCERRADA COM SUCESSO!!")
+
 if __name__ == "__main__":
     banco = Database()
-    banco.insert_produto()
-
+    banco.update_produto(1)
